e17912e7cdbe4625268a8d96e9eae42e532d2c36
TheBlueOompaLoompa <blueoompaloompa72@gmail.com>
2021-09-04 21:46:54 -0500

    Rainbow screen

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -13,6 +13,15 @@ struct BootInfo {
 	uint64_t mMapDescSize;
 } ;
 
+unsigned int colors[] = {
+    0xff0000,
+    0xffa500,
+    0xffff00,
+    0x00ff00,
+    0x0000ff,
+    0x52307c
+};
+
 extern "C" void _start(BootInfo* bootInfo){
     
     BasicRenderer renderer = BasicRenderer(bootInfo->framebuffer, bootInfo->psf1_Font); 
@@ -21,9 +30,14 @@ extern "C" void _start(BootInfo* bootInfo){
     
     //renderer.Print(to_string(GetMemorySize(bootInfo->mMap, mMapEntries, bootInfo->mMapDescSize)));
 
-    renderer.Color = 0xff0000;
+    renderer.Color = 0x000000;
 
-    renderer.Rect(10, 10, 10, 10);
+    for(unsigned long x = 0L; x < (renderer.TargetFramebuffer->Width/10); x++){
+        for(unsigned long i = 0L; i < 10; i++) {
+            renderer.Rect(((x*10L)+i), 0, 1, renderer.TargetFramebuffer->Height);
+        }
+        renderer.Color = colors[x%6];
+    }
 
     while(true) {}
     
